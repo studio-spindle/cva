fd55bd0c5819b5c2064441d00158e01aba311c55
Vledder <remi@studiospindle.nl>
2020-03-22 17:01:40 +0100

    update uri internal weaviate

--- client/src/apollo/apolloClient.tsx
+++ client/src/apollo/apolloClient.tsx
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'cross-fetch';
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:8080/v1/graphql',
+  uri: '//weaviate.creatingvalue.co/v1/graphql',
   fetch,
 });
 
