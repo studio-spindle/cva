9a3037333b4db8df5c77ee13e45ca4a9362eef59
Vledder <remi@studiospindle.nl>
2020-03-29 14:54:43 +0200

    temporary disable blogposts until content is added

--- client/src/pages/nl.tsx
+++ client/src/pages/nl.tsx
@@ -1,23 +1,31 @@
 import { NextPage } from 'next';
-import { Container } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 import Page from '../templates/Page';
-import { Data, PostBlog } from '../shared.types';
-import usePosts from '../hooks/usePosts';
-import BlogList from '../components/BlogList';
-import Loading from '../components/Loading';
+// import { Data, PostBlog } from '../shared.types';
+// import usePosts from '../hooks/usePosts';
+// import BlogList from '../components/BlogList';
+// import Loading from '../components/Loading';
 
-const Blog: NextPage = () => {
-  const posts: Data<PostBlog>[] | null = usePosts('NL');
-  return (
-    <Page title="Netherlands" siteTitle="Netherlands">
-      <Container maxWidth="md">
-        {posts === null && <Loading />}
-        {posts && (
-          <BlogList posts={posts} />
-        )}
-      </Container>
-    </Page>
-  );
-};
+const Blog: NextPage = () => (
+  <Page title="Netherlands" siteTitle="Netherlands">
+    <Container maxWidth="md">
+      <Typography>Coming soon</Typography>
+    </Container>
+  </Page>
+);
+
+// const Blog: NextPage = () => {
+//   const posts: Data<PostBlog>[] | null = usePosts('NL');
+//   return (
+//     <Page title="Netherlands" siteTitle="Netherlands">
+//       <Container maxWidth="md">
+//         {posts === null && <Loading />}
+//         {posts && (
+//           <BlogList posts={posts} />
+//         )}
+//       </Container>
+//     </Page>
+//   );
+// };
 
 export default Blog;
