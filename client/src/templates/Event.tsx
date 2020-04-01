d76e49486c83a4152b8ef28134b5d5246ae9ba19
Vledder <remi@studiospindle.nl>
2020-04-01 17:51:45 +0200

    use yarn instead of npm in Docker, remove redundant @types, fix lint error

--- client/src/templates/Event.tsx
+++ client/src/templates/Event.tsx
@@ -337,8 +337,8 @@ const Event: FC<EventTemplateProps> = ({ data }) => {
                 The organizing committee of the Third Global Conference, June 2020, on Creating
                 Value is inviting academics and professionals to share their latest insights and/or
                 research findings on the topic of Creating Value through a paper, poster or video
-                presentation, or any other creative way to share the knowledge that is suitable for a
-                conference setting.
+                presentation, or any other creative way to share the knowledge that is suitable for
+                a conference setting.
               </Typography>
             </InfoBlock>
           </Grid>
