f5ea1c65ff44788f024d9777b1a32d243a205933
Bob van Luijt <bob@semi.technology>
2020-04-06 09:32:21 +0200

    Fix URL for Weaviate

--- client/src/apollo/apolloClient.tsx
+++ client/src/apollo/apolloClient.tsx
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 import fetch from 'cross-fetch';
 
 const apolloClient = new ApolloClient({
-  uri: '//weaviate.creatingvaluealliance.com/v1/graphql',
+  uri: '//weaviate.creatingvalue.co/v1/graphql',
   fetch,
 });
 
