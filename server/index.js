37745df004fe48ad959779cf71cb01b6ae39b7fb
Vledder <remi@studiospindle.nl>
2020-04-18 19:38:42 +0200

    add URL

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000' // TODO: add the production url
+  origin: process.env.ENV === 'production' ? '*.creatingvalue.co' : 'http://localhost:3000'
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
