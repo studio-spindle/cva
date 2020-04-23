54ef41691630b4b53406c73cf331b6527d0cad6d
Vledder <remi@studiospindle.nl>
2020-04-23 12:21:20 +0200

    use script to create .env file

--- server/script.sh
+++ server/script.sh
@@ -5,7 +5,7 @@
 #
 # Available variables are the keys in the json file. E.g., $EMAIL_TO_1
 ##
-cat env.json | eval $(cat env.json | jq -r '. | to_entries | .[] | .key + "=\"" + .value + "\""')
+cat env.json | jq -r '. | to_entries | .[] | .key + "=\"" + .value + "\""' >> .env
 
 # serve yarn
 yarn serve
