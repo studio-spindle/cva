7c38e69ac1c534cf4a87d934208130e7c42059fe
Bob van Luijt <bob@semi.technology>
2020-04-23 09:56:55 +0200

    Update script and Dockerfile

--- server/script.sh
+++ server/script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+##
 # sets the variables
+#
+# Available variables are the keys in the json file. E.g., EMAIL_TO_1
+#
+##
 cat env.json | eval $(cat env.json | jq -r '. | to_entries | .[] | .key + "=\"" + .value + "\""')
 
 # serve yarn
