cb4dd6798863dde851c4bbbca08f046f64963a02
Vledder <remi@studiospindle.nl>
2020-06-23 10:04:07 +0200

    adjust way for building sitemap

--- client/next.config.js
+++ client/next.config.js
@@ -1,17 +1,5 @@
 module.exports = {
-  webpack(_config, { isServer }) {
-    const config = { ..._config };
-    if (isServer) {
-      const originalEntry = config.entry;
-
-      config.entry = async () => {
-        const entries = { ...(await originalEntry()) };
-        entries['./scripts/build-sitemap.js'] = './scripts/build-sitemap.js';
-
-        return entries;
-      };
-    }
-
+  webpack(config) {
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
