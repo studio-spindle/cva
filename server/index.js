6bd801f9d37986fd99d8505c8be8a52eb9877a7d
Vledder <remi@studiospindle.nl>
2020-07-13 16:22:56 +0200

    server: add error handling for CORS

--- server/index.js
+++ server/index.js
@@ -8,7 +8,19 @@ const clientHostname = process.env.NODE_ENV === 'development' ? 'http://localhos
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: clientHostname
+  origin: function(origin, callback) {
+    if (!origin) {
+      var msg = 'No origin has been set.';
+      return callback(new Error(msg), false);
+    }
+
+    if(clientHostname !== origin){
+      var msg = `The CORS policy for this site does not allow access from ${origin}.`;
+      return callback(new Error(msg), false);
+    }
+
+    return callback(null, true);
+  }
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
