47fd4bf180f88ffc9be494de069f81e3a0cade46
Vledder <remi@studiospindle.nl>
2020-07-17 09:51:27 +0200

    change cors setting (+lint)

--- server/app.js
+++ server/app.js
@@ -1,32 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
-const clientHostname = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://creatingvalue.co';
+const clientHostname =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://creatingvalue.co";
+
+const whitelist = ["http://localhost:3000", "https://creatingvalue.co"];
 
 console.log(clientHostname);
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) {
-      var msg = 'No origin has been set.';
-      return callback(new Error(msg), false);
-    }
-
-    if(clientHostname !== origin){
-      var msg = `The CORS policy for this site does not allow access from ${origin}.`;
-      return callback(new Error(msg), false);
-    }
-
-    return callback(null, true);
-  }
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        var msg = !origin
+          ? "CORS: origin is not set."
+          : `The CORS policy for this site does not allow access from ${origin}.`;
+        callback(
+          new Error(msg)
+        );
+      }
+    },
+  })
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Routes
-app.use('/subscribe', require('./routes/subscribe'));
+app.use("/subscribe", require("./routes/subscribe"));
 
 module.exports = app;
